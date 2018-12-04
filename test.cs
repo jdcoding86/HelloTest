4f978cfbfb0aebeb15d10838d4f8db094a0885fd
jdcoding86 <45607448+jdcoding86@users.noreply.github.com>
2018-12-04 14:13:30 -0600

    Update test.cs
    test stuff branch

--- test.cs
+++ test.cs
@@ -2,7 +2,7 @@ public class MyStuff
     {
         public string MyWork()
         {
-            var data = "somejunkstuff";
+            var data = "somejunkstuff12345";
             return data;
         }
 
